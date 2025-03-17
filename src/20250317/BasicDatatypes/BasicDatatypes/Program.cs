69d8a03a7cc9433507996487c534a266cdb1bc05
Atilla Kati (WIFI) <atilla@kati.bz>
2025-03-17 21:35:26 +0100

    Example BasicDataTypes

---
+++ src/20250317/BasicDatatypes/BasicDatatypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDatatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int => integer values
+
+
+            //declaration
+            int age;
+
+            //initialize
+            age = -1;
+
+            //declaration & initialize
+            int count = 0;
+
+            Console.WriteLine("Age: " + age.ToString());
+
+            //dezimal values
+            double weight = 10.50;
+
+            //money
+            decimal money = 15.99m;
+        }
+    }
+}
