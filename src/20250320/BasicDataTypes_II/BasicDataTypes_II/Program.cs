bf371453ffe775a6e2370149fb6584804db24a74
Atilla Kati (WIFI) <atilla@kati.bz>
2025-03-20 19:31:59 +0100

    Example for basic datatypes, part 2

---
+++ src/20250320/BasicDataTypes_II/BasicDataTypes_II/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDataTypes_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int, double, dezimal
+
+            //string
+
+            string name = "Gandalf";
+            //name = string.Empty;  
+            //name = "";
+
+            char sign = '7';
+
+            bool isPowerOn = false;
+
+            
+        }
+    }
+}
