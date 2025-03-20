473d6fb99d4e788d48911830759bdd0c16f483ca
Atilla Kati (WIFI) <atilla@kati.bz>
2025-03-20 19:32:30 +0100

    Exercise with basic datatypes, calculations and output

---
+++ src/20250320/ExCircularCalculation/ExCircularCalculation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExCircularCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            //variable declaration & initialisation
+            double diameter = 15.12;    //Durchmesser
+            double circumference = 0.0; //Umfang
+
+            //calculation
+            circumference = diameter * Math.PI;
+
+            //output
+            Console.WriteLine("The circumferenc of my circle is: " + circumference.ToString());
+        }
+    }
+}
