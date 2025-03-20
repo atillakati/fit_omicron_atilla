75036d227288d0fd4e5afd354d5379c36d9fcb0d
Atilla Kati (WIFI) <atilla@kati.bz>
2025-03-20 21:25:14 +0100

    Indrodution into handling user inputs in console

---
+++ src/20250320/UserInput/UserInput/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            string userInput = string.Empty;
+            int yearOfBirth = 0;
+            int age = 0;
+
+            //read user name
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+
+            //read year of birth
+            Console.Write("Hello, " + name);
+            Console.WriteLine(" please enter the year of your birth: ");
+            userInput = Console.ReadLine();
+
+            //implicilty ==> indirekt, unbeabsichtigt
+            //explicit ==> klar definiert
+
+            //convert user input into a numeric value
+            yearOfBirth = int.Parse(userInput);            
+
+            //calculate the age
+            age = DateTime.Now.Year - yearOfBirth;
+
+            //output all user info          
+            Console.WriteLine(name + " your are " + age + " years old.");
+
+        }
+    }
+}
