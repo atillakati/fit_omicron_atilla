fb0e0ed5ae04e6ca818adf0af0d5c192ea9e0076
Atilla Kati (WIFI) <atilla@kati.bz>
2025-03-31 17:06:18 +0200

    Undo changes @StudentManagementApp_V1

--- src/20250327/StudentManagementApp_V1/StudentManagementApp_V1/Program.cs
+++ src/20250327/StudentManagementApp_V1/StudentManagementApp_V1/Program.cs
@@ -25,25 +25,30 @@ namespace StudentManagementApp_V1
         static void Main(string[] args)
         {
             string userInput = string.Empty;
+            string fullName = string.Empty;
+            DateTime birthday = DateTime.MinValue;
+            int postalCode = 0;
+            string city = string.Empty;
             bool userInputIsInvalid = false;
-            int studenCount = 0;
-
-            string[] studenFullNames;
-            DateTime[] studenBirthdays;
-            int[] studenPostalCodes;
-            string[] studenCities;
 
 
-            //retrieve count of students
+            //student data input
+            Console.WriteLine("Please enter following information:");
+            Console.WriteLine();
+            Console.Write("\tYour full name:             ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            fullName = Console.ReadLine();
+
             do
             {
                 Console.ResetColor();
-                Console.Write("Please enter the count of your students: ");
+                Console.Write("\tYour birthday (dd.mm.yyyy): ");
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 userInput = Console.ReadLine();
+
                 try
                 {
-                    studenCount = int.Parse(userInput);
+                    birthday = DateTime.Parse(userInput);
                     userInputIsInvalid = false;
                 }
                 catch (Exception ex)
@@ -54,83 +59,40 @@ namespace StudentManagementApp_V1
             }
             while (userInputIsInvalid);
 
-            //dimension the arrays
-            studenFullNames = new string[studenCount];
-            studenBirthdays = new DateTime[studenCount];
-            studenCities = new string[studenCount];
-            studenPostalCodes = new int[studenCount];
-
-            for (int i = 0; i < studenCount; i++)
+            do
             {
-                //student data input
                 Console.ResetColor();
-                Console.WriteLine("Please enter following information for student #{0}:", i + 1);
-                Console.WriteLine();
-
-                //get student name
-                Console.Write("\tYour full name:             ");
+                Console.Write("\tPostal code:                ");
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                studenFullNames[i] = Console.ReadLine();
-
-                //get student birthday
-                do
+                userInput = Console.ReadLine();
+                try
                 {
-                    Console.ResetColor();
-                    Console.Write("\tYour birthday (dd.mm.yyyy): ");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    userInput = Console.ReadLine();
-
-                    try
-                    {
-                        studenBirthdays[i] = DateTime.Parse(userInput);
-                        userInputIsInvalid = false;
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("ERROR: " + ex.Message);
-                        userInputIsInvalid = true;
-                    }
+                    postalCode = int.Parse(userInput);
+                    userInputIsInvalid = false;
                 }
-                while (userInputIsInvalid);
-
-                //get student postal code
-                do
+                catch (Exception ex)
                 {
-                    Console.ResetColor();
-                    Console.Write("\tPostal code:                ");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    userInput = Console.ReadLine();
-                    try
-                    {
-                        studenPostalCodes[i] = int.Parse(userInput);
-                        userInputIsInvalid = false;
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("ERROR: " + ex.Message);
-                        userInputIsInvalid = true;
-                    }
+                    Console.WriteLine("ERROR: " + ex.Message);
+                    userInputIsInvalid = true;
                 }
-                while (userInputIsInvalid);
-
-                //get student city
-                Console.ResetColor();
-                Console.Write("\tCity:                       ");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                studenCities[i] = Console.ReadLine();
             }
+            while (userInputIsInvalid);
+
+            Console.ResetColor();
+            Console.Write("\tCity:                       ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            city = Console.ReadLine();
 
             Console.ResetColor();
 
             //output student data
-            for (int i = 0; i < studenCount; i++)
+            if (!userInputIsInvalid)
             {
-                Console.WriteLine("\nStudent #{0}:", i + 1);
-                Console.WriteLine("Name: " + studenFullNames[i]);
-                Console.WriteLine(studenPostalCodes[i].ToString() + " " + studenCities[i]);
-                Console.WriteLine("Day of birth: " + studenBirthdays[i].ToShortDateString());
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Name: " + fullName);
+                Console.WriteLine(postalCode.ToString() + " " + city);
+                Console.WriteLine("Day of birth: " + birthday.ToShortDateString());
             }
-
         }
     }
 }
