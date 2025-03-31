2e037f2c48c24db444c1950bf239a732498f6e4d
Atilla Kati (WIFI) <atilla@kati.bz>
2025-03-31 21:04:29 +0200

    Custom types - starting with struct basics

---
+++ src/20250331/StructBasics/StructBasics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructBasics
+{     
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+
+            //declaration
+            Student mike;
+            Student[] studentList = new Student[5];
+
+            //create the object
+            mike = new Student();
+
+            //init
+            mike.Name = "Mike";
+            mike.City = "Lustenau";
+            mike.Birthday = new DateTime(1980, 4, 1);
+            mike.PostalCode = 6930;            
+
+            Console.WriteLine(mike.Name);
+
+            
+        }
+
+    }
+
+
+}
