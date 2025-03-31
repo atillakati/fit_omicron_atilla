4b531269a1e3daea449081b05fb17ceac504c9e9
Atilla Kati (WIFI) <atilla@kati.bz>
2025-03-31 21:04:55 +0200

    Custom types - refactoring existing Student Management App V2

---
+++ src/20250331/StudentManagementApp_v2/StudentManagementApp_v2/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentManagementApp_v2
+{
+    public struct Student
+    {
+        public string Name;
+        public string City;
+        public DateTime Birthday;
+        public int PostalCode;
+    }
+}
