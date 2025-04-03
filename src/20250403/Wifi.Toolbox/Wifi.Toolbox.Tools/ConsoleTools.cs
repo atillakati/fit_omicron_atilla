12cf10fd8f25b44613da8e13a1c8a17ab0594802
Atilla Kati (WIFI) <atilla@kati.bz>
2025-04-03 21:39:20 +0200

    Initial creation of a WIFI library

---
+++ src/20250403/Wifi.Toolbox/Wifi.Toolbox.Tools/ConsoleTools.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.Toolbox.Tools
+{
+    public abstract class ConsoleTools
+    {
+        public static int GetInt(string inputPrompt)
+        {
+            string userInput = string.Empty;
+            int intValue = 0;
+            bool userInputIsInvalid = false;
+
+            do
+            {
+                Console.ResetColor();
+                Console.Write(inputPrompt);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                userInput = Console.ReadLine();
+
+                try
+                {
+                    intValue = int.Parse(userInput);
+                    userInputIsInvalid = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERROR: " + ex.Message);
+                    userInputIsInvalid = true;
+                }
+            }
+            while (userInputIsInvalid);
+
+            Console.ResetColor();
+
+            return intValue;
+        }
+    }
+}
