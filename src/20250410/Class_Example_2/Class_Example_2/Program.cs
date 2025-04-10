026c0ddcb523b845e906f21b4e5d2821d5b0201f
Atilla Kati (WIFI) <atilla@kati.bz>
2025-04-10 21:47:52 +0200

    c'tor & Encapsulation- Part1

---
+++ src/20250410/Class_Example_2/Class_Example_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Example_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle myCar = new Vehicle("Badmobil Black Edition V18", 250);            
+            
+            myCar.Display();
+
+            myCar.SpeedUp(120);
+            //myCar.CurrentSpeed = 320;                        
+            myCar.Display();
+            
+            int speed = myCar.CurrentSpeed * 150;            
+
+            myCar.Color = ConsoleColor.Yellow;
+            Console.WriteLine("Color of my car: " + myCar.Color);
+
+            myCar.Display();
+        }
+    }
+}
