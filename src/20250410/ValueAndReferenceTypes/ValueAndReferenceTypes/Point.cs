e7e1c0f687d59e7e1744a227e65c353fa3a59b54
Atilla Kati (WIFI) <atilla@kati.bz>
2025-04-10 19:34:01 +0200

    Value - and Reference Types

---
+++ src/20250410/ValueAndReferenceTypes/ValueAndReferenceTypes/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndReferenceTypes
+{
+    public class Point
+    {
+        public int XPos;
+        public int YPos;
+    }
+}
---
+++ src/20250410/ValueAndReferenceTypes/ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndReferenceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p3;  //null            
+
+            Point p2 = new Point();
+            Point p1 = new Point();
+            p1.XPos = 5;
+            p1.YPos = 2;
+
+            //copy the point
+            p2 = p1;
+
+            p1.XPos = 9;
+            p1.YPos = 10;
+
+            //show the point contents
+            Display(p1);
+            Display(p2);
+        }
+
+        static void Display(Point pointToDisplay)
+        {
+            Console.WriteLine($"Point: ({pointToDisplay.XPos},{pointToDisplay.YPos})");
+
+            pointToDisplay.XPos = 0;
+        }
+    }
+}
