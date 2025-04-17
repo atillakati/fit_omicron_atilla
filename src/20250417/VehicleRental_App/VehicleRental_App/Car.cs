6724b7a195360d412fa2531f3100dfb2b205baf0
Atilla Kati (WIFI) <atilla@kati.bz>
2025-04-17 18:52:27 +0200

    Inheritance - IS A relation between class types

---
+++ src/20250417/VehicleRental_App/VehicleRental_App/Car.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace VehicleRental_App
+{
+    public class Car : Vehicle
+    {
+		private int _seatCount;
+
+        public Car(string description, string brand, int enginePower, int maxSpeed, int seatCount)
+            : base(description, brand, enginePower, maxSpeed)
+        {
+            _seatCount = seatCount;
+        }
+
+        public int SeatCount
+		{
+			get { return _seatCount; }		
+		}
+
+	}
+}
---
+++ src/20250417/VehicleRental_App/VehicleRental_App/Program.cs
@@ -0,0 +1,41 @@
+﻿using BetterConsoleTables;
+using System;
+
+namespace VehicleRental_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vw_golf = new Car("Golf White Edition V6", "VW", 180, 220, 5);
+
+            var carList = new Vehicle[]
+            {
+                vw_golf,
+                new Vehicle("Demo Vehicle", "Brand New", 90, 110),
+                new Vehicle("Future Edition", "BadMobil", 250, 280),
+                new Car("Vectra 2.0i", "Opel", 110, 185, 5),
+                new Scooter("Joy Rider", "Segway", 10, 15.0)
+            };
+            
+            //do something with all vehicles
+            foreach (var car in carList)
+            {
+                car.SpeedUp(100);
+                Console.WriteLine(car.GetInfoString());
+            }
+
+            Console.WriteLine();
+
+            ShowCarsAsTable(carList);
+        }
+
+        private static void ShowCarsAsTable(Vehicle[] carList)
+        {
+            var table = new Table();
+            table.From(carList);
+
+            Console.WriteLine(table.ToString());
+        }
+    }
+}
