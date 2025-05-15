277944ac256d90a858e7ecb7e72e6281bc946255
Atilla Kati (WIFI) <atilla@kati.bz>
2025-05-15 21:35:49 +0200

    Implement Playlist unit tests

---
+++ src/wifi-playlist-editor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Core.Test/PlaylistTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.PlaylistEditor.Core.Test
+{
+    [TestFixture]
+    public class PlaylistTests
+    {
+        private Playlist _fixture;
+
+        [Test]
+        public void Name_get()
+        {
+            //arrange
+            _fixture = new Playlist("Gandalf", "NUnit Tests");
+
+            //act
+            var result = _fixture.Name;
+
+            //assert
+            Assert.That(result, Is.EqualTo("Gandalf"));
+        }
+
+        [Test]
+        public void Author_get()
+        {
+            //arrange
+            _fixture = new Playlist("Gandalf", "NUnit Tests");
+
+            //act
+            var result = _fixture.Author;
+
+            //assert
+            Assert.That(result, Is.EqualTo("NUnit Tests"));
+        }
+
+
+        [Test]
+        public void TestSomething()
+        {
+            //AAA
+            //arrange
+            int result = 0;
+            int value1 = 40;
+            int value2 = 50;
+
+            //act
+            result = value1 + value2;
+
+            //assert
+            Assert.That(result, Is.EqualTo(90));
+        }
+    }
+}
