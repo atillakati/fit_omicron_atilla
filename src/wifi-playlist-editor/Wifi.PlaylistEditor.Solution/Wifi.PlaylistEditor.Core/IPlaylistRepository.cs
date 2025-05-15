60930734db8ae9c1c8a50e20dc2bea5ecccf8f16
Atilla Kati (WIFI) <atilla@kati.bz>
2025-05-15 21:35:16 +0200

    Implement some core types

---
+++ src/wifi-playlist-editor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Core/IPlaylistRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Wifi.PlaylistEditor.Core
+{
+
+    /// <summary>
+    /// m3u, pls, json
+    /// </summary>
+    public interface IPlaylistRepository
+    {
+        string Extension { get; }
+        string Description { get; }
+
+        Playlist Load(string playlistPath);
+
+        bool Save(string playlistPath, Playlist playlistToSave);
+    }
+}
---
+++ src/wifi-playlist-editor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Core/Playlist.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.PlaylistEditor.Core
+{
+    public class Playlist
+    {
+		private string _name;
+		private string _author;
+		private DateTime _createdAt;
+        private List<IPlaylistItem> _itemList;		
+
+        public Playlist(string name, string author)
+        {
+            _name = name;
+            _author = author;
+			_createdAt = DateTime.Now;
+
+			_itemList = new List<IPlaylistItem>();
+        }
+
+
+		public TimeSpan Duration
+		{
+			get
+			{
+				var duration = _itemList.Sum(x => x.Duration.TotalSeconds);				
+				return TimeSpan.FromSeconds(duration);
+			}
+		}
+
+        public DateTime CreatedAt
+		{
+			get { return _createdAt; }			
+		}
+
+		public IEnumerable<IPlaylistItem> ItemList
+		{
+			get { return _itemList; }			
+		}
+
+		public string Author
+		{
+			get { return _author; }			
+		}
+
+		public string Name
+		{
+			get { return _name; }			
+		}
+
+
+		public void Add(IPlaylistItem newItem)
+		{
+			_itemList.Add(newItem);
+		}
+
+        public void Remove(IPlaylistItem itemToRemove)
+        {
+            _itemList.Remove(itemToRemove);
+        }        
+
+		public void Clear()
+		{
+			_itemList.Clear();
+		}		
+    }
+}
