55cf21ebd2213c0dddf7b32634a3ad9330770347
Atilla Kati (WIFI) <atilla@kati.bz>
2025-05-19 21:45:30 +0200

    Create MongoDbRepository class with tests

---
+++ src/wifi-playlist-editor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Repositories/Database/MongoRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Wifi.PlaylistEditor.Repositories.Database
+{
+    public class MongoRepository<T> where T : class
+    {
+        private readonly IMongoCollection<T> _collection;
+
+        public MongoRepository(string connectionString, string databaseName, string collectionName)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString)) throw new ArgumentNullException(nameof(connectionString));
+            if (string.IsNullOrWhiteSpace(databaseName)) throw new ArgumentNullException(nameof(databaseName));
+            if (string.IsNullOrWhiteSpace(collectionName)) throw new ArgumentNullException(nameof(collectionName));
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            _collection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task CreateAsync(T item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            await _collection.InsertOneAsync(item);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _collection.Find(FilterDefinition<T>.Empty).ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentNullException(nameof(id));
+
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            return await _collection.Find(filter).FirstOrDefaultAsync();            
+        }
+
+        public async Task UpdateAsync(string id, T updatedItem)
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentNullException(nameof(id));
+            if (updatedItem == null) throw new ArgumentNullException(nameof(updatedItem));
+
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            await _collection.ReplaceOneAsync(filter, updatedItem);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentNullException(nameof(id));
+
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            await _collection.DeleteOneAsync(filter);
+        }
+    }
+}
---
+++ src/wifi-playlist-editor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Repositories/Database/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace Wifi.PlaylistEditor.Repositories.Database
+{    
+    public class Person
+    {
+        [BsonId] // Marks as MongoDB document ID
+        [BsonRepresentation(BsonType.ObjectId)] // Stores as ObjectId but allows passing string
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+}
